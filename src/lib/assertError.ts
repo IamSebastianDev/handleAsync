2018292ca93dee674d8073325a3e98a08f617523
Sebastian <sebastian@iamsebastian.dev>
2023-05-10 23:18:07 +0200

    chore: improve docs

--- src/lib/assertError.ts
+++ src/lib/assertError.ts
@@ -1,7 +1,13 @@
 /** @format */
 
 import { isError, convertUnknownToError } from '../utils';
-
+/**
+ * Asserts that a value is an instance of `Error`. If it's not, the function
+ * converts the value to an `Error` instance using the `convertUnknownToError` function.
+ *
+ * @param {unknown} error The value to assert as an `Error`.
+ * @returns {Error} The `Error` instance.
+ */
 export const assertError = (error: unknown): Error => {
     if (isError(error)) return error;
 
--- src/utils/convertUnknownToError.util.ts
+++ src/utils/convertUnknownToError.util.ts
@@ -2,6 +2,20 @@
 
 import { isErrorLike } from './isErrorLike.typeguard';
 
+/**
+ * Converts a value of unknown type to an `Error`. If the value is an `Error`-like object,
+ * the function returns a new `Error` with the properties of the `Error`-like object and
+ * `name` set to 'Unknown Exception'.
+ *
+ * If the value is not an `Error`-like object, the function tries to stringify it and use
+ * the resulting string as the message of a new `Error`. If stringifying the value throws
+ * an error, the function falls back to converting the value to a string using the `String`
+ * constructor.
+ *
+ * @param {unknown} unknownCaught The value to convert to an `Error`.
+ * @returns {Error} The resulting `Error`.
+ */
+
 export const convertUnknownToError = (unknownCaught: unknown): Error => {
     if (isErrorLike(unknownCaught)) return { ...unknownCaught, name: 'Unknown Exception' };
 
